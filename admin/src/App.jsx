65cea7af3ac13974cba61c2099a0f49292798ff1
Shreyash <shreyashjanbandhu7@gmail.com>
2025-08-25 21:55:50 +0530

    the work regarding to the admin has been started

---
+++ admin/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-2xl text-purple-500'>Hello Admin Shreyash</h1>
+    </div>
+  )
+}
+
+export default App
